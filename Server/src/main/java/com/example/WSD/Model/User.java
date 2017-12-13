a276850116b831c01492caa4ed04b0f2c6e672e1
jedrek18 <stilo001@gmail.com>
2017-12-14 00:37:15 +0100

    dependencies update

--- Server/src/main/java/com/example/WSD/Model/User.java
+++ Server/src/main/java/com/example/WSD/Model/User.java
@@ -2,7 +2,6 @@ package com.example.WSD.Model;
 
 import javax.persistence.Id;
 import org.springframework.stereotype.Component;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
